2d8669b094b2dfa228c0f44bea16ae89c21e4bc9
Fire Cube <ben7@gmx.ch>
2025-11-08 16:26:06 +0100

    General: Argument parsing & debug dumping

--- src/lexer.h
+++ src/lexer.h
@@ -1,6 +1,11 @@
 #pragma once
 
-#include <filesystem>
+#include <string>
+
+#include <cereal/archives/json.hpp>
+#include <cereal/types/string.hpp>
+
+#include <magic_enum/magic_enum.hpp>
 
 #include "types.h"
 
@@ -27,6 +32,15 @@ struct Token {
     u32 line;
     u32 column;
     u32 length;
+
+    template <class Archive>
+    void serialize(Archive& archive) {
+        archive(cereal::make_nvp("type", std::string{ magic_enum::enum_name(type) }),
+                cereal::make_nvp("lexeme", lexeme),
+                cereal::make_nvp("line", line),
+                cereal::make_nvp("column", column),
+                cereal::make_nvp("length", length));
+    }
 };
 
 int lex(std::vector<std::string>& inputLines, std::vector<Token>& tokens);
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <fstream>
 #include <iostream>
 
+#include <argparse/argparse.hpp>
+
+#include <cereal/archives/json.hpp>
+#include <cereal/types/vector.hpp>
+
 #include "lexer.h"
 
-std::filesystem::path inputPath = "../../asm_examples/hello_world.asm";
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser argumentParser("AsmCube");
 
-int main() {
-    inputPath = std::filesystem::absolute(inputPath);
+    argumentParser.add_argument("inputFile")
+        .help("path to input file")
+        .required();
+
+    argumentParser.add_argument("--dump")
+        .help("dumps the output by lexer and parser as json to separate files")
+        .default_value(false)
+        .implicit_value(true);
+
+    try {
+        argumentParser.parse_args(argc, argv);
+    }
+    catch (const std::exception& error) {
+        std::cerr << error.what() << std::endl;
+        std::cerr << argumentParser;
+        std::exit(1);
+    }
+
+    std::filesystem::path inputPath = std::filesystem::absolute(argumentParser.get<std::string>("inputFile"));
     if (!std::filesystem::exists(inputPath)) {
         std::cout << std::format("File '{}' does not exist!\n", inputPath.string());
         return 1;
@@ -17,12 +40,21 @@ int main() {
         std::cout << std::format("Failed to open file '{}'\n", inputPath.string());
         return 1;
     }
+
     std::vector<std::string> inputLines;
     std::string line;
     while (std::getline(in, line)) {
         inputLines.push_back(line);
     }
+
     std::vector<Token> tokens;
     lex(inputLines, tokens);
+
+    if (argumentParser["--dump"] == true) {
+        std::ofstream out("lex.json", std::ios::binary);
+        cereal::JSONOutputArchive archive(out);
+        archive(cereal::make_nvp("tokens", tokens));
+    }
+
     return 0;
 }
