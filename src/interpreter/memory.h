39ba161ae31b5b63995c125f49009d2729d9c261
Fire Cube <ben7@gmx.ch>
2026-08-05 21:23:23 +0200

    Interpreter: add fast paths for read- and writeMemory, optimize setPermission

--- src/interpreter/memory.h
+++ src/interpreter/memory.h
@@ -34,39 +34,66 @@ class Memory {
         std::unordered_map<u64, Page> pages;
 
     public:
+        Page& getPage(const u64 address) {
+            const u64 pageIndex = address / PageSize;
+
+            auto [it, inserted] = pages.try_emplace(pageIndex);
+            if (inserted && address >= UINT64_MAX - 8_MiB) {
+                setPermission(pageIndex * PageSize, PageSize, Permission{ true, true, false });
+            }
+            return it->second;
+        }
+
         template <std::unsigned_integral T>
         void writeMemory(const u64 address, const T& data) {
+            // fast path if all data is in one page
+            u32 offset = address % PageSize;
             u32 dataSize = sizeof(data);
+
+            if (offset + dataSize <= PageSize) {
+                Page& page = getPage(address);
+                for (u32 i = 0; i < dataSize; ++i) {
+                    if (!page.permissionWrite.test(offset + i)) {
+                        LOG_ERROR("Write access violation at address 0x{:016x}", address + i);
+                    }
+                    page.data[offset + i] = data >> (8 * i) & 0xFF;
+                    page.initialized.set(offset + i);
+                }
+                return;
+            }
+
+            // slow path
             for (u32 i = 0; i < dataSize / sizeof(u8); ++i) {
                 u32 offset = (address + i) % PageSize;
-                // If address in stack space
-                Page* page = nullptr;
-                if (address >= UINT64_MAX - 8_MiB) {
-                    if (!pages.contains((address + i) / PageSize)) {
-                        page = &pages[(address + i) / PageSize];
-                        setPermission((address + i) - ((address + i ) % PageSize), PageSize, Permission{ true, true, false });
-                    }
-                    else {
-                        page = &pages[(address + i) / PageSize];
-                    }
-                }
-                else {
-                    page = &pages[(address + i) / PageSize];
-                }
-                if (!page->permissionWrite.test(offset)) {
+                Page& page = getPage(address + i);
+
+                if (!page.permissionWrite.test(offset)) {
                    LOG_ERROR("Write access violation at address 0x{:016x}", address + i);
                 }
-                page->data[offset] = data >> (8 * i) & 0xFF;
-                page->initialized.set(offset);
+                page.data[offset] = data >> (8 * i) & 0xFF;
+                page.initialized.set(offset);
             }
         }
 
         template <std::unsigned_integral T>
         void writeMemoryNoExcept(const u64 address, const T& data) {
+            // fast path if all data is in one page
+            u32 offset = address % PageSize;
             u32 dataSize = sizeof(data);
+
+            if (offset + dataSize <= PageSize) {
+                Page& page = getPage(address);
+                for (u32 i = 0; i < dataSize; ++i) {
+                    page.data[offset + i] = data >> (8 * i) & 0xFF;
+                    page.initialized.set(offset + i);
+                }
+                return;
+            }
+
+            // slow path
             for (u32 i = 0; i < dataSize / sizeof(u8); ++i) {
                 u32 offset = (address + i) % PageSize;
-                Page& page = pages[(address + i) / PageSize];
+                Page& page = getPage(address + i);
                 page.data[offset] = data >> (8 * i) & 0xFF;
                 page.initialized.set(offset);
             }
@@ -74,68 +101,92 @@ class Memory {
 
         template <std::unsigned_integral T>
         void readMemory(const u64 address, T& data) {
-            data = 0;
+            // fast path if all data is in one page
+            u32 offset = address % PageSize;
             u32 dataSize = sizeof(data);
+
+            if (offset + dataSize <= PageSize) {
+                Page& page = getPage(address);
+                data = 0;
+                for (u32 i = 0; i < dataSize; ++i) {
+                    if (!page.permissionRead.test(offset + i)) {
+                        LOG_ERROR("Read access violation at address 0x{:016x}", address + i);
+                    }
+                    if (logLevel == LogLevel::Debug && !page.initialized.test(offset + i)) {
+                        LOG_DEBUG("Reading uninitialized memory at address 0x{:016x}", address + i);
+                    }
+                    data |= static_cast<T>(page.data[offset + i]) << (8 * i);
+                }
+                return;
+            }
+
+            // slow path
+            data = 0;
             for (u32 i = 0; i < dataSize / sizeof(u8); ++i) {
-                u32 offset = (address + i) % PageSize;
-                // If address in stack space
-                Page* page = nullptr;
-                if (address >= UINT64_MAX - 8_MiB) {
-                    if (!pages.contains((address + i) / PageSize)) {
-                        page = &pages[(address + i) / PageSize];
-                        setPermission((address + i) - ((address + i ) % PageSize), PageSize, Permission{ true, true, false });
-                    }
-                    else {
-                        page = &pages[(address + i) / PageSize];
-                    }
-                }
-                else {
-                    page = &pages[(address + i) / PageSize];
-                }
-                if (!page->permissionRead.test(offset)) {
+                offset = (address + i) % PageSize;
+                Page& page = getPage(address + i);
+                if (!page.permissionRead.test(offset)) {
                    LOG_ERROR("Read access violation at address 0x{:016x}", address + i);
                 }
-                if (!page->initialized.test(offset)) {
+                if (logLevel == LogLevel::Debug && !page.initialized.test(offset)) {
                    LOG_DEBUG("Reading uninitialized memory at address 0x{:016x}", address + i);
                 }
-                data |= static_cast<T>(page->data[offset]) << (8 * i);
+                data |= static_cast<T>(page.data[offset]) << (8 * i);
             }
         }
 
         template <std::unsigned_integral T>
         void readMemoryNoExcept(const u64 address, T& data) {
-            data = 0;
+            // fast path if all data is in one page
+            u32 offset = address % PageSize;
             u32 dataSize = sizeof(data);
+
+            if (offset + dataSize <= PageSize) {
+                Page& page = getPage(address);
+                data = 0;
+                for (u32 i = 0; i < dataSize; ++i) {
+                    data |= static_cast<T>(page.data[offset + i]) << (8 * i);
+                }
+                return;
+            }
+
+            // slow path
+            data = 0;
             for (u32 i = 0; i < dataSize / sizeof(u8); ++i) {
                 u32 offset = (address + i) % PageSize;
-                Page& page = pages[(address + i) / PageSize];
+                Page& page = getPage(address + i);
                 data |= static_cast<T>(page.data[offset]) << (8 * i);
             }
         }
 
         void setPermission(const u64 address, const u64 size, Permission permission) {
-            for (u64 n = 0; n < size; ++n) {
-                u64 i = address + n;
-                u32 offset = i % PageSize;
-                Page& page = pages[i / PageSize];
+            u64 current = address;
+            u64 remaining = size;
+            while (remaining > 0) {
+                Page& page = pages[current / PageSize];
+                const u64 offset = current % PageSize;
+                const u64 count = std::min(remaining, PageSize - offset);
+                for (u64 n = 0; n < count; ++n) {
+                    if (permission.read) {
+                        page.permissionRead.set(offset + n);
+                    } else {
+                        page.permissionRead.reset(offset + n);
+                    }
 
-                if (permission.read) {
-                    page.permissionRead.set(offset);
-                } else {
-                    page.permissionRead.reset(offset);
-                }
+                    if (permission.write) {
+                        page.permissionWrite.set(offset + n);
+                    } else {
+                        page.permissionWrite.reset(offset + n);
+                    }
 
-                if (permission.write) {
-                    page.permissionWrite.set(offset);
-                } else {
-                    page.permissionWrite.reset(offset);
+                    if (permission.execute) {
+                        page.permissionExecute.set(offset + n);
+                    } else {
+                        page.permissionExecute.reset(offset + n);
+                    }
                 }
-
-                if (permission.execute) {
-                    page.permissionExecute.set(offset);
-                } else {
-                    page.permissionExecute.reset(offset);
-                }
+                current += count;
+                remaining -= count;
             }
         }
 
